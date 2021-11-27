bbf34715e7301433ba17819a7b382cc7a7d7bb2d
鲁铖锴 <1390813832@qq.com>
2021-11-27 20:10:56 +0800

    1

--- src/main/java/com/luck/HbaseShard.java
+++ src/main/java/com/luck/HbaseShard.java
@@ -50,44 +50,44 @@ public class HbaseShard {
         operateService.createTable(tableName,series);
 
         //添加数据集---按rowKey
-        long startTime=System.currentTimeMillis(); //获取开始时间
-        for (Map<String, Object> cell : trajectoryInfos) {
-            //            logUtil.prepareLog(list);
-
-            BaseInfo baseInfo = new BaseInfo();
-            List<String> columnFamilyList = new ArrayList<>();
-            List<Map<String, Object>> columnsList = new ArrayList<>();
-            String rowKey = "111";
-
-//          System.out.print(list.get(j));
-            try {
-                columnFamilyList.add("data");
-                columnsList.add(cell);
-            } catch (Exception e) {
-//                hbaseShardInsert.error("插入报错rowKey:" + rowKey);
-            }
-
-            baseInfo.setRowKey(rowKey);
-            baseInfo.setColumnFamilyList(columnFamilyList);
-            baseInfo.setColumnsList(columnsList);
-            operateService.addByRowKey(baseInfo);
-        }
-        long endTime=System.currentTimeMillis(); //获取结束时间
-        logUtil.runTimeLog("addAll", endTime, startTime);
-
-        //查看表中所有数据
-        long startTime2=System.currentTimeMillis(); //获取开始时间
-        ResultScanner rs = operateService.getValueByTable();
-        long endTime2=System.currentTimeMillis(); //获取结束时间
-        logUtil.runTimeLog("getValueByTable", endTime2, startTime2);
-//        logUtil.getValueByTable(rs);
-
-        //根据rowKey前缀查询记录
-        long startTime3=System.currentTimeMillis(); //获取开始时间
-        ResultScanner rsByKey = operateService.getValueByPreKey("黑MN7991");
-        long endTime3=System.currentTimeMillis(); //获取结束时间
-        logUtil.runTimeLog("getValueByPreKey", endTime3, startTime3);
-        logUtil.getValueByTable(rsByKey);
+//        long startTime=System.currentTimeMillis(); //获取开始时间
+//        for (Map<String, Object> cell : trajectoryInfos) {
+//            //            logUtil.prepareLog(list);
+//
+//            BaseInfo baseInfo = new BaseInfo();
+//            List<String> columnFamilyList = new ArrayList<>();
+//            List<Map<String, Object>> columnsList = new ArrayList<>();
+//            String rowKey = "111";
+//
+////          System.out.print(list.get(j));
+//            try {
+//                columnFamilyList.add("data");
+//                columnsList.add(cell);
+//            } catch (Exception e) {
+////                hbaseShardInsert.error("插入报错rowKey:" + rowKey);
+//            }
+//
+//            baseInfo.setRowKey(rowKey);
+//            baseInfo.setColumnFamilyList(columnFamilyList);
+//            baseInfo.setColumnsList(columnsList);
+//            operateService.addByRowKey(baseInfo);
+//        }
+//        long endTime=System.currentTimeMillis(); //获取结束时间
+//        logUtil.runTimeLog("addAll", endTime, startTime);
+//
+//        //查看表中所有数据
+//        long startTime2=System.currentTimeMillis(); //获取开始时间
+//        ResultScanner rs = operateService.getValueByTable();
+//        long endTime2=System.currentTimeMillis(); //获取结束时间
+//        logUtil.runTimeLog("getValueByTable", endTime2, startTime2);
+////        logUtil.getValueByTable(rs);
+//
+//        //根据rowKey前缀查询记录
+//        long startTime3=System.currentTimeMillis(); //获取开始时间
+//        ResultScanner rsByKey = operateService.getValueByPreKey("黑MN7991");
+//        long endTime3=System.currentTimeMillis(); //获取结束时间
+//        logUtil.runTimeLog("getValueByPreKey", endTime3, startTime3);
+//        logUtil.getValueByTable(rsByKey);
     }
 
 }
