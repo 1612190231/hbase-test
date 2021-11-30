3aea7df9bf7590275889dc4fda53e4a24d0e26ac
鲁铖锴 <1390813832@qq.com>
2021-11-30 20:59:08 +0800

    验证分布

--- src/main/java/com/luck/HbaseShard.java
+++ src/main/java/com/luck/HbaseShard.java
@@ -1,7 +1,5 @@
 package com.luck;
 
-import com.luck.entity.BaseInfo;
-import com.luck.entity.PointInfo;
 import com.luck.entity.TrajectoryInfo;
 import com.luck.service.HbaseShardService;
 import com.luck.service.OperateService;
@@ -9,15 +7,12 @@ import com.luck.service.impl.HbaseShardServiceImpl;
 import com.luck.service.impl.OperateServiceImpl;
 import com.luck.utils.CsvUtil;
 import com.luck.utils.LogUtil;
-import org.apache.hadoop.hbase.client.ResultScanner;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.Collator;
 import java.text.ParseException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * @author luchengkai
@@ -28,13 +23,13 @@ public class HbaseShard {
 
     public static void main(String[] args) throws MalformedURLException, ParseException {
         //日志类加载
-        LogUtil logUtil = new LogUtil();
+//        LogUtil logUtil = new LogUtil();
 
         //获取数据源, 轨迹合并
         HbaseShardService hbaseShardService = new HbaseShardServiceImpl();
 //        File file = new File("file:////home/cklu/data/shard/test_s.csv");
-        File file = new File("src/main/resources/test_s.csv");
-        URL url = new URL("file:////C:/Users/user/Desktop/code/hbase-test/src/main/resources/test_s.csv");
+        String str = "-1130.csv";
+        URL url = new URL("file:////C://Users//13908//Desktop//code//hbase-test//src//main//resources//" + str);
         List<TrajectoryInfo> trajectoryInfos = hbaseShardService.getTrajectoryInfos(url);
 //        List<OrgVo> orgList = new ArrayList();
 //        trajectoryInfos = trajectoryInfos.stream().filter(TrajectoryInfo -> TrajectoryInfo.getPointInfos().size() > 1).collect(Collectors.toList());
@@ -47,21 +42,28 @@ public class HbaseShard {
 
         // 构造索引
         trajectoryInfos = hbaseShardService.dimensionReduction(trajectoryInfos);
+        List<String> keys = new ArrayList<String>();
+        for(TrajectoryInfo trajectoryInfo: trajectoryInfos){
+            String key = trajectoryInfo.getVehicleNo() + ',' + trajectoryInfo.getKeyTime() + ',' + trajectoryInfo.getKeyRange();
+            keys.add(key);
+        }
+        File file = new File("src/main/resources/keys" + str);
+        CsvUtil.exportCsv(file, keys);
 
-        //开始hbase操作
-        //初始化
-        OperateService operateService = new OperateServiceImpl();
-        operateService.setSeries("data");
-        operateService.setTableName("hbase_shard");
-        operateService.init();
-
-        //创建表
-        String series = operateService.getSeries();
-        String tableName = operateService.getTableName();
-        byte[][] startKey = new byte[5][];
-        operateService.createTable(tableName,series, startKey);
-
-        //添加数据集---按rowKey
+//        //开始hbase操作
+//        //初始化
+//        OperateService operateService = new OperateServiceImpl();
+//        operateService.setSeries("data");
+//        operateService.setTableName("hbase_shard");
+//        operateService.init();
+//
+//        //创建表
+//        String series = operateService.getSeries();
+//        String tableName = operateService.getTableName();
+//        byte[][] startKey = new byte[5][];
+//        operateService.createTable(tableName,series, startKey);
+//
+//        //添加数据集---按rowKey
 //        long startTime=System.currentTimeMillis(); //获取开始时间
 //        for (Map<String, Object> cell : trajectoryInfos) {
 //            //            logUtil.prepareLog(list);
--- src/main/java/com/luck/utils/CsvUtil.java
+++ src/main/java/com/luck/utils/CsvUtil.java
@@ -8,7 +8,8 @@ package com.luck.utils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
-import java.io.IOException;
+
+import java.io.*;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -37,5 +38,59 @@ public class CsvUtil {
             throw new RuntimeException("Error reading GDELT data:", e);
         }
         return result;
+    }
+
+    /**
+     * 读取
+     * @param file csv文件(路径+文件名)，csv文件不存在会自动创建
+     * @param dataList 数据
+     * @return
+     */
+    public static boolean exportCsv(File file, List<String> dataList){
+        boolean isSucess=false;
+
+        FileOutputStream out=null;
+        OutputStreamWriter osw=null;
+        BufferedWriter bw=null;
+        try {
+            out = new FileOutputStream(file);
+            osw = new OutputStreamWriter(out);
+            bw =new BufferedWriter(osw);
+            if(dataList!=null && !dataList.isEmpty()){
+                for(String data : dataList){
+                    bw.append(data).append("\r");
+                }
+            }
+            isSucess=true;
+        } catch (Exception e) {
+            isSucess=false;
+        }finally{
+            if(bw!=null){
+                try {
+                    bw.close();
+                    bw=null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(osw!=null){
+                try {
+                    osw.close();
+                    osw=null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(out!=null){
+                try {
+                    out.close();
+                    out=null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return isSucess;
     }
 }
