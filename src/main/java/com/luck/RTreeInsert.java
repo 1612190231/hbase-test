e69380ae3e66afa5e7b326c5ce52f77bbbf93667
luck <luchengkai@obase.com.cn>
2023-03-02 04:21:23 +0800

    完善r*+tree

--- src/main/java/com/luck/RTreeInsert.java
+++ src/main/java/com/luck/RTreeInsert.java
@@ -1,5 +1,10 @@
 package com.luck;
 
+import com.github.davidmoten.rtreemulti.Entry;
+import com.github.davidmoten.rtreemulti.RTree;
+import com.github.davidmoten.rtreemulti.geometry.Rectangle;
+import com.github.davidmoten.rtreemulti.internal.EntryDefault;
+import com.luck.entity.PointInfo;
 import com.luck.entity.TrajectoryInfo;
 import com.luck.service.HbaseShardService;
 import com.luck.service.impl.HbaseShardServiceImpl;
@@ -7,21 +12,108 @@ import com.luck.utils.LogUtil;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.*;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RTreeInsert {
-    public static void main(String[] args) throws MalformedURLException, ParseException {
+    public static void main(String[] args) throws MalformedURLException, ParseException, SQLException, ClassNotFoundException {
         //日志类加载
+        Class.forName("com.mysql.jdbc.Driver");
         LogUtil logUtil = new LogUtil();
 
         //获取数据源, 轨迹合并
         HbaseShardService hbaseShardService = new HbaseShardServiceImpl();
-//        URL url = new URL("file:////C:\\Users\\user\\Desktop\\code\\hbase-test\\src\\main\\resources\\-1010.csv");
+        URL url = new URL("file:////E:\\Desktop\\code\\hbase-test\\src\\main\\resources\\test.csv");
 //        URL url = new URL("file:////root/data/test/data/-210830.csv");
-        URL url = new URL(args[0]);
+//        URL url = new URL(args[0]);
         List<TrajectoryInfo> trajectoryInfos = hbaseShardService.getTrajectoryInfos(url);
         logUtil.print("load url: " + url);
+        PreparedStatement ps;
+        PreparedStatement ps1;
+        Connection connection = DriverManager.getConnection("jdbc:mysql://114.117.4.200:3306/final_paper?createDatabaseIfNotExist=true&useSSL=false", "luck", "luck");
+//        connection.setAutoCommit(false);
+        // insert traj
+        try {
+            int count = 0;
+            String sql = "insert into trajInfo values (?,?,?,?,?,?,?,?)";
+            ps = connection.prepareStatement(sql);
+            for (TrajectoryInfo trajectoryInfo: trajectoryInfos) {
+                count++;
+                ps.setInt(1, Integer.parseInt(trajectoryInfo.getKeyTime()));
+                ps.setString(2, trajectoryInfo.getVehicleNo());
+                ps.setDouble(3, trajectoryInfo.getMinLat());
+                ps.setDouble(4, trajectoryInfo.getMaxLat());
+                ps.setDouble(5, trajectoryInfo.getMinLon());
+                ps.setDouble(6, trajectoryInfo.getMaxLon());
+                ps.setLong(7, trajectoryInfo.getMinTime());
+                ps.setLong(8, trajectoryInfo.getMaxTime());
+                ps.addBatch();
+                if (count % 1000 == 0) {
+                    ps.executeBatch();
+                    logUtil.print("trajs commit, count=" + count);
+                }
+            }
+            count = 0;
+            String sql1 = "insert into pointInfo values (?,?,?,?,?)";
+            ps1 = connection.prepareStatement(sql1);
+            for (TrajectoryInfo trajectoryInfo: trajectoryInfos) {
+                count++;
+                for (PointInfo pointInfo: trajectoryInfo.getPointInfos()) {
+                    ps1.setInt(1, Integer.parseInt(trajectoryInfo.getKeyTime()));
+                    ps1.setString(2, trajectoryInfo.getVehicleNo());
+                    ps1.setString(3, pointInfo.getUtc());
+                    ps1.setDouble(4, pointInfo.getLat());
+                    ps1.setDouble(5, pointInfo.getLon());
+                    ps1.addBatch();
+                }
+                if (count % 1000 == 0) {
+                    ps1.executeBatch();
+                    logUtil.print("points commit, count=" + count);
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
+        // build rtree
+        String keyTime = "";
+        String sql = "select * from trajInfo";
+        Statement st = connection.createStatement();
+        ResultSet resultSet = st.executeQuery(sql);
+//        List<TrajectoryInfo> trajectoryInfos1 = new ArrayList<>();
+        List<Entry<String, Rectangle>> entries = new ArrayList<Entry<String, Rectangle>>(10000);
+        Map<String, RTree<String, Rectangle>> treeMap = new HashMap<>();
+        while (resultSet.next()) {
+            TrajectoryInfo trajectoryInfo = new TrajectoryInfo();
+            trajectoryInfo.setKeyTime(String.valueOf(resultSet.getInt(1)));
+            trajectoryInfo.setVehicleNo(resultSet.getString(2));
+            trajectoryInfo.setMinLat(resultSet.getDouble(3));
+            trajectoryInfo.setMaxLat(resultSet.getDouble(4));
+            trajectoryInfo.setMinLon(resultSet.getDouble(5));
+            trajectoryInfo.setMaxLon(resultSet.getDouble(6));
+            trajectoryInfo.setMinTime(resultSet.getLong(7));
+            trajectoryInfo.setMaxTime(resultSet.getLong(8));
+//            trajectoryInfos1.add(trajectoryInfo);
+
+            if (keyTime.equalsIgnoreCase("")) {
+                keyTime = trajectoryInfo.getKeyTime();
+                Rectangle rectangle = Rectangle.create(trajectoryInfo.getMinLon(), trajectoryInfo.getMinLat(), trajectoryInfo.getMaxLon(), trajectoryInfo.getMaxLat());
+                entries.add(new EntryDefault<String, Rectangle>(trajectoryInfo.getKeyTime() + trajectoryInfo.getVehicleNo(), rectangle));
+            }
+            else if (!keyTime.equalsIgnoreCase(trajectoryInfo.getKeyTime())){
+                RTree<String, Rectangle> rTree = RTree.maxChildren(4).create(entries);
+                treeMap.put(trajectoryInfo.getKeyTime(), rTree);
+                logUtil.print("build rtree success.");
+            }
+            else {
+                Rectangle rectangle = Rectangle.create(trajectoryInfo.getMinLon(), trajectoryInfo.getMinLat(), trajectoryInfo.getMaxLon(), trajectoryInfo.getMaxLat());
+                entries.add(new EntryDefault<String, Rectangle>(trajectoryInfo.getKeyTime() + trajectoryInfo.getVehicleNo(), rectangle));
+            }
+        }
+        logUtil.print("build rtree list success.");
     }
 }
--- src/main/java/com/luck/RTreeSelect.java
+++ src/main/java/com/luck/RTreeSelect.java
@@ -1,7 +1,97 @@
 package com.luck;
 
-public class RTreeSelect {
-    public static void main(String[] args) {
+import com.github.davidmoten.rtreemulti.Entry;
+import com.github.davidmoten.rtreemulti.RTree;
+import com.github.davidmoten.rtreemulti.geometry.Rectangle;
+import com.github.davidmoten.rtreemulti.internal.EntryDefault;
+import com.luck.entity.PointInfo;
+import com.luck.entity.TrajectoryInfo;
+import com.luck.service.HbaseShardService;
+import com.luck.service.impl.HbaseShardServiceImpl;
+import com.luck.utils.LogUtil;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RTreeSelect {
+    public static void main(String[] args) throws MalformedURLException, ParseException, SQLException, ClassNotFoundException {
+        //日志类加载
+        Class.forName("com.mysql.jdbc.Driver");
+        LogUtil logUtil = new LogUtil();
+
+        //获取数据源, 轨迹合并
+        HbaseShardService hbaseShardService = new HbaseShardServiceImpl();
+        URL url = new URL("file:////E:\\Desktop\\code\\hbase-test\\src\\main\\resources\\test.csv");
+//        URL url = new URL("file:////root/data/test/data/-210830.csv");
+//        URL url = new URL(args[0]);
+        List<TrajectoryInfo> trajectoryInfos = hbaseShardService.getTrajectoryInfos(url);
+        logUtil.print("load url: " + url);
+        PreparedStatement ps;
+        PreparedStatement ps1;
+        Connection connection = DriverManager.getConnection("jdbc:mysql://114.117.4.200:3306/final_paper?createDatabaseIfNotExist=true&useSSL=false", "luck", "luck");
+//        connection.setAutoCommit(false);
+
+        // build rtree
+        String keyTime = "";
+        String sql = "select * from trajInfo";
+        Statement st = connection.createStatement();
+        ResultSet resultSet = st.executeQuery(sql);
+//        List<TrajectoryInfo> trajectoryInfos1 = new ArrayList<>();
+        List<Entry<String, Rectangle>> entries = new ArrayList<Entry<String, Rectangle>>(10000);
+        Map<Integer, RTree<String, Rectangle>> treeMap = new HashMap<>();
+        while (resultSet.next()) {
+            TrajectoryInfo trajectoryInfo = new TrajectoryInfo();
+            trajectoryInfo.setKeyTime(String.valueOf(resultSet.getInt(1)));
+            trajectoryInfo.setVehicleNo(resultSet.getString(2));
+            trajectoryInfo.setMinLat(resultSet.getDouble(3));
+            trajectoryInfo.setMaxLat(resultSet.getDouble(4));
+            trajectoryInfo.setMinLon(resultSet.getDouble(5));
+            trajectoryInfo.setMaxLon(resultSet.getDouble(6));
+            trajectoryInfo.setMinTime(resultSet.getLong(7));
+            trajectoryInfo.setMaxTime(resultSet.getLong(8));
+//            trajectoryInfos1.add(trajectoryInfo);
+
+            if (keyTime.equalsIgnoreCase("")) {
+                keyTime = trajectoryInfo.getKeyTime();
+                Rectangle rectangle = Rectangle.create(trajectoryInfo.getMinLon(), trajectoryInfo.getMinLat(), trajectoryInfo.getMaxLon(), trajectoryInfo.getMaxLat());
+                entries.add(new EntryDefault<String, Rectangle>(trajectoryInfo.getKeyTime() + trajectoryInfo.getVehicleNo(), rectangle));
+            }
+            else if (!keyTime.equalsIgnoreCase(trajectoryInfo.getKeyTime())){
+                RTree<String, Rectangle> rTree = RTree.maxChildren(4).create(entries);
+                treeMap.put(Integer.parseInt(trajectoryInfo.getKeyTime()), rTree);
+                logUtil.print("build rtree success.");
+            }
+            else {
+                Rectangle rectangle = Rectangle.create(trajectoryInfo.getMinLon(), trajectoryInfo.getMinLat(), trajectoryInfo.getMaxLon(), trajectoryInfo.getMaxLat());
+                entries.add(new EntryDefault<String, Rectangle>(trajectoryInfo.getKeyTime() + trajectoryInfo.getVehicleNo(), rectangle));
+            }
+        }
+        logUtil.print("build rtree list success.");
+        //处理range
+        Double minLon = Double.valueOf(args[0]);
+        Double maxLon = Double.valueOf(args[1]);
+        Double minLat = Double.valueOf(args[2]);
+        Double maxLat = Double.valueOf(args[3]);
+        Rectangle qRectangle = Rectangle.create(minLon, minLat, maxLon, maxLat);
+        //处理time
+        String sTime = args[4];
+        String eTime = args[5];
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        long init_date = df.parse("01/01/2020 00:00:00").getTime();
+        int days_s = (int)((df.parse(sTime).getTime() - init_date) / (1000 * 60 * 60 * 24));
+        int days_e = (int)((df.parse(eTime).getTime() - init_date) / (1000 * 60 * 60 * 24));
+        for (int i = days_s; i <= days_e; i++) {
+            RTree<String, Rectangle> rTree = treeMap.get(i);
+            Iterable<com.github.davidmoten.rtreemulti.Entry<String, Rectangle>> result = rTree.search(qRectangle);
+            logUtil.print("search rTree success.");
+        }
     }
 }
